208cf9252591cd82ab9126c7b45f9dc3369cea14
Imandra McKenzie <imckenzie@squarespace.com>
2024-10-18 18:40:53 -0700

    Add basic files & functionality to grab PDX tournament info

---
+++ cmd/hm-drafter/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const (
+	apiTemplate = "https://kqhivemind.com/api/tournament/%s/?format=json%s%s"
+	apiFormFieldsSlug = "player-info-field"
+	apiTeamsSlug = "team"
+	apiPlayersSlug = "player"
+	apiAllTournamentsSlug = "tournament"
+	scene = "kqpdx"
+	maxPages              = 5
+)
+
+type Tournament struct {
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	Date      string `json:"date"`
+	SceneName string `json:"scene_name"`
+}
+
+type APIResponse struct {
+	Results []Tournament `json:"results"`
+}
+
+// type player struct {
+// 	Name     string   `json:"name"`
+// 	Skill    string   `json:skill`
+// 	Roles    []string `json:roles`
+// 	Captain  string   `json:captain`
+// }
+
+// getPDXTournies takes the API string that lists all tournaments, checks the `results` list for entries where the `scene_name` is `kqpdx` and returns those events with their ID, name, and date in nested lists
+func getPDXTournies(slug string) (portlandTournies [][]string) {
+	client := &http.Client{}
+
+	for page := 1; page <= maxPages; page++ {
+		// Construct API URL with the slug and page number
+		api := fmt.Sprintf(apiTemplate, slug, fmt.Sprintf("&page=%d", page), "")
+		log.Printf("Tournament API: %v", api)
+		
+		req, err := http.NewRequest("GET", api, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Defer the closing of the body
+		defer resp.Body.Close()
+
+		// Decode the response into APIResponse struct
+		var apiResponse APIResponse
+		if err := json.NewDecoder(resp.Body).Decode(&apiResponse); err != nil {
+			log.Fatal(err)
+		}
+
+		// Loop through each item in `results` and filter by `scene_name == "kqpdx"`
+		for _, tournament := range apiResponse.Results {
+			if tournament.SceneName == scene {
+				portlandTournies = append(portlandTournies, []string{
+					fmt.Sprintf("%d", tournament.ID), tournament.Name, tournament.Date,
+				})
+			}
+		}
+	}
+
+	return portlandTournies
+}
+
+func main() {
+	portlandTournies := getPDXTournies(apiAllTournamentsSlug)
+	for _, tourney := range portlandTournies {
+		fmt.Printf("ID: %s, Name: %s, Date: %s\n", tourney[0], tourney[1], tourney[2])
+	}
+}
