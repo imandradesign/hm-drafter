30c9b0d84307941c75f8a652baea2646f20c4e94
Imandra McKenzie <imckenzie@squarespace.com>
2024-10-23 15:06:47 -0700

    Specify index filename

--- cmd/hm-drafter/main.go
+++ cmd/hm-drafter/main.go
@@ -34,7 +34,7 @@ func main() {
 	router.Use(gin.Logger())
 
 	// Load HTML templates
-	router.LoadHTMLFiles("templates/*.html")
+	router.LoadHTMLFiles("templates/index.html")
 
 	router.GET("/", func(c *gin.Context) {
 		// Fetch tournament data
